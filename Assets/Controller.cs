b3fa290487cf17f18f24080567bf485243335ce5
wang-yichun <wang-yichun@meyougames.com>
2015-05-14 22:02:43 +0800

    1st commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//using System.Drawing;
+//using System.IO;
+
+public class Controller : MonoBehaviour
+{
+	
+	public void button_clicked ()
+	{
+		Debug.Log ("button_clicked");
+		
+		//		Image image = Image.FromFile("/Users/leadprogrammer/Documents/bigmap.png");
+		//
+		//		byte[] a = imageToByteArray (image);
+
+		StartCoroutine (WaitLoad ("file:///Users//leadprogrammer//Documents//UnityProject//MatchHole//pic_a//1.png"));
+
+//		GameObject obj = GameObject.Find ("MySprite");
+//		obj.renderer.material.mainTexture = wwwTexture;
+//		obj.renderer.material.mainTexture = wwwTexture.texture;
+	}
+	
+	//	public byte[] imageToByteArray(System.Drawing.Image imageIn)
+	//	{
+	//		MemoryStream ms = new MemoryStream();
+	//		imageIn.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+	//		return ms.ToArray();
+	//	}
+
+	IEnumerator WaitLoad (string fileName)
+	{
+		WWW wwwTexture = new WWW (fileName);
+		
+		Debug.Log (wwwTexture.url);
+		
+		yield return wwwTexture;
+
+		Texture2D texture = wwwTexture.texture;
+		Color color = texture.GetPixel (600, 500);
+
+		RawImage myImage = GameObject.Find ("MyRawImage").GetComponent<RawImage> ();
+		myImage.texture = texture;
+	}
+}
---
+++ Assets/ImageGestureHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using TouchScript.Gestures;
+using TouchScript.Hit;
+
+public class ImageGestureHandler : MonoBehaviour {
+
+	void OnEnable ()
+	{
+		GetComponent<TapGesture> ().Tapped += tappedHandler;
+	}
+	
+	void OnDisable ()
+	{
+		GetComponent<TapGesture> ().Tapped -= tappedHandler;
+	}
+
+	void tappedHandler (object sender, EventArgs e)
+	{
+		Vector2 res = new Vector2 ();
+		ITouchHit hit;
+		(sender as TapGesture).GetTargetHitResult(out hit);
+		ITouchHit2D hit2d = hit as ITouchHit2D;
+
+		Debug.Log (hit2d.Point);
+	}
+}
